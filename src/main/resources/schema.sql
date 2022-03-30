443a24b11a13427cd988be0320ad26e40198addf
Gatene1 <Gatene1@iCloud.com>
2022-03-29 21:59:26 -0400

    Updated wallpaper that went awry.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -9,7 +9,7 @@ CREATE TABLE "coloridentity" (
 );
 CREATE TABLE "rarity" (
      "TypesId" INT NOT NULL PRIMARY KEY,
-     "Type" VARCHAR NOT NULL
+     "Type" INT NOT NULL
 );
 
 CREATE TABLE "cards" (
